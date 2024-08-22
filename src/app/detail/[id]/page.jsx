2391aba6888d1448818d87451c347fa948291702
hayyantahirr <hayyantahir7@gmail.com>
2024-08-23 03:47:13 +0500

    code updated

--- src/app/detail/[id]/page.jsx
+++ src/app/detail/[id]/page.jsx
@@ -1,20 +1,17 @@
-
 import axios from "axios";
 import Generate from "./generate";
-
-
 
 const detail = async ({ params }) => {
   const { id } = params;
   console.log("id is" + id);
- const res = await axios(`https://api.imgflip.com/get_memes`);
-  const meme=await res.data.data.memes;
- console.log(meme);
- const memeSelected = meme.find((meme) => meme.id === id);
+  const res = await axios(`https://api.imgflip.com/get_memes`);
+  const meme = await res.data.data.memes;
+  console.log(meme);
+  const memeSelected = meme.find((meme) => meme.id === id);
 
   return (
     <>
-     <Generate memeSelected={memeSelected}/>
+      <Generate memeSelected={memeSelected} />
     </>
   );
 };
